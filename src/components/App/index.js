b6b67cd794fdcf031efe850c2850bd40e3761b1a
Tom Giddings <tom.giddings@gmail.com>
2019-08-10 00:59:04 +0100

    Add repository link

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -37,7 +37,7 @@ class App extends Component {
               </CSSTransition>
             </TransitionGroup>
           )}/>
-          <footer>Designed by Tom Giddings and built in React.js. <a href="https://github.com/tomgiddings">View the code on Github</a>.</footer>
+          <footer>Designed by Tom Giddings and built in React.js. <a href="https://github.com/tomgiddings/tomgiddings">View the code on Github</a>.</footer>
         </div>
       </BrowserRouter>
     )
