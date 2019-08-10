fe3d73d2824a6be81b43df4e576e135732bddf5c
Tom Giddings <tom.giddings@gmail.com>
2019-08-10 23:13:37 +0100

    Tweak footer content

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Footer() {
   return (
-    <footer>Designed by Tom Giddings and built in React.js. <a href="https://github.com/tomgiddings/tomgiddings">View the code on Github</a>.</footer>
+    <footer>Designed and built by Tom Giddings using React.js. <a href="https://github.com/tomgiddings/tomgiddings">View the code on Github</a>.</footer>
   );
 }
 export default Footer;
