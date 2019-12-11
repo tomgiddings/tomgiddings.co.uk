24e8e627f9a34b3b168bc4473b31faffc0aa4b29
Tom Giddings <tom.giddings@gmail.com>
2019-12-11 10:03:23 +0000

    Change location to be clearer

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -10,7 +10,7 @@ class Home extends React.Component<*> {
         <div className="content-wrapper">
           <div>
             <h1><img src="/me.jpg" alt="Me" className="profile-photo" />Hi there!</h1>
-            <h2>I'm Tom, a Web Developer &amp; Broadcast Engineer from Bristol.</h2>
+            <h2>I'm Tom, a Web Developer &amp; Broadcast Engineer based in Buckinghamshire.</h2>
           </div>
           <ul>
             <li><a href="https://github.com/tomgiddings"><i className="fab fa-4x fa-github-square"></i></a></li>
