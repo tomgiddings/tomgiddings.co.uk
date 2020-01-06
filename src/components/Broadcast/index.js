c7ba52d8f14da4d1a042c9ff73b37f43a2e67605
Tom Giddings <tom.giddings@gmail.com>
2020-01-06 15:25:21 +0000

    Update broadcast content, remove old images

--- src/components/Broadcast/index.js
+++ src/components/Broadcast/index.js
@@ -10,12 +10,17 @@ class Broadcast extends React.Component<*> {
         <div className="content-wrapper">
           <h1>Broadcast Engineer</h1>
           <p>I've worked as an engineer from Student and Community Radio through to national brands including Absolute Radio, Kiss, Magic, Planet Rock, and Kerrang! Radio.</p>
-          <p>In 2017, I became one of the founding directors of <a href="https://www.safepairofhands.co.uk">Safe Pair of Hands Technology</a>, focussing on broadcast technology and IT.</p>
+          <p>My work has included:</p>
           <ul>
-            <li>Studio installation and configuration.</li>
-            <li>Worked on Outside Broadcasts including Snowbombing Festival, The Brit Awards, and the Isle of Wight Festival.</li>
-            <li>Provided support including out of hours work.</li>
+            <li>Technical installation and refurbishment of broadcast studios.</li>
+            <li>Outside Broadcasts including Snowbombing Festival, The Brit Awards, and the Isle of Wight Festival.</li>
+            <li>Helping to maintain broadcast and IT systems, from studio hardware to VMware vSphere environments.</li>
+            <li>Supporting brands both during and out of hours.</li>
           </ul>
+          <p>I'm currently part of <a href="https://www.safepairofhands.co.uk">Safe Pair of Hands Technology</a>, focussing on broadcast technology and IT.</p>
+          <a href="https://www.safepairofhands.co.uk">
+            <img src="/logos/spohlogo.png" alt="Safe Pair of Hands Technology" />
+          </a>
         </div>
       </article>
     );
